4b466336eb177a8614ca9ca9d2a9af4614ca2b5f
DEPSTRCZ <jiri.edelmann@gmail.com>
2024-05-27 14:11:18 +0200

    scrap orm

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -1,24 +1,8 @@
 from typing import Union
 from fastapi import FastAPI
-from tortoise.models import Model
-from tortoise import fields, Tortoise
-import asyncio
 
 app = FastAPI()
 
-
-async def init():
-    # Here we create a SQLite DB using file "db.sqlite3"
-    #  also specify the app name of "models"
-    #  which contain models from "app.models"
-    await Tortoise.init(
-        db_url='mysql://moniaq:admin@localhost:3306/moniaq',
-        modules={'models': ['models']}
-    )
-    # Generate the schema
-    await Tortoise.generate_schemas()
-
-asyncio.run(init())
 
 
 @app.get("/")
