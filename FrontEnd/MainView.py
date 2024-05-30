8290d065f1f725bc09848ce8cee662430ab9ee1e
Alex Smolik <alexsmolik47@gmail.com>
2024-05-30 12:56:02 +0200

    basic sensor view, advanced sensor view (90% done, lacks map)

--- FrontEnd/MainView.py
+++ FrontEnd/MainView.py
@@ -4,20 +4,3 @@ st.title("MONAQ")
 st.header("Vizualizace kvality vzduchu.")
 st.divider()
 st.info("Momentálně není vybrán žádný senzor")
-
-
-left, mid, right = st.columns([5, 10, 5],gap="small")
-with st.sidebar:
-    st.title("Vyberte senzor")
-    for id in range(10):
-        container = st.container(border=True)
-        container.header(f"Senzor {id}")
-        container.button("Zobrazit",key=id)
-with left:
-    st.write()
-
-with mid:
-    st.write()
-
-with right:
-    st.write()
--- FrontEnd/ViewSensor.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.set_page_config(page_title="MONAQ - Senzor", page_icon="📈")
-
-st.write("Senzor: 0")
